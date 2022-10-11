4252422bcbc7e799a3db8f80d0173bb2de2dd554
Csokán Pál András <andrejmoltok@gmail.com>
2022-10-11 21:51:38 +0200

    Delete test.js

--- test.js
+++
@@ -1,9 +0,0 @@
-// for (let i = 1; i <= 30; i++) {
-//     console.log(i + " Gergely nap = ", Math.round(i/1.8)," Lana nap");
-// }
-
-// for (let i = 1; i <= 31; i++) {
-//     if (i % 6 == 0) {
-//         console.log(i);
-//     }
-// }
